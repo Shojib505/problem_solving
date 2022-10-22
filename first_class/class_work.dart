346309d0ae0c1d4caa55769e356bca7a97848cfb
alish <alishojib12@gmail.com>
2022-10-22 15:40:44 +0600

    logic for five numbers with if else

--- first_class/class_work.dart
+++ first_class/class_work.dart
@@ -1,3 +1,21 @@
 void main(List<String> args) {
   print('hello');
+
+  int a = 8;
+  int b = 6;
+  int c = 12;
+  int d = 10;
+  int e = 15;
+
+  if (a > b && a > c && a > d && a > e) {
+    print('a is bigger');
+  } else if (b > a && b > c && b > d && b > e) {
+    print('b is bigger');
+  } else if (c > a && c > b && c > d && b > e) {
+    print('c is bigger');
+  } else if (d > a && d > b && d > c && d > e) {
+    print('d is bigger');
+  } else {
+    print('e is bigger');
+  }
 }
