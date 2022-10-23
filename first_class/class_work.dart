6d59b13a65b9ca90a0022987d87ae0a785fededd
alish <alishojib12@gmail.com>
2022-10-23 16:15:10 +0600

    tali jura better apporch ad manual later

--- first_class/class_work.dart
+++ first_class/class_work.dart
@@ -1,7 +1,7 @@
 void main(List<String> args) {
   print('hello');
 
-  int a = 8;
+  /* int a = 8;
   int b = 6;
   int c = 12;
   int d = 10;
@@ -17,5 +17,21 @@ void main(List<String> args) {
     print('d is bigger');
   } else {
     print('e is bigger');
-  }
+  } */
+
+  ///Better Approch
+  List<int> numb = [15, 80, 10, -5, 50, 65, 90];
+  numb.sort();
+  //TODO: do it menual later
+
+  //// for (int i = 0; i < numb.length; i++) {
+  ////   int  temp = i;
+  ////   if (numb[temp] < numb[i + 1]) {
+  //     // int temp2 = numb[i+1];
+  ////   } else {
+  // //    print('not bigger lo');
+  //  // }
+  // //}
+
+  print('temp is bigger ${numb[numb.length - 1]}');
 }
